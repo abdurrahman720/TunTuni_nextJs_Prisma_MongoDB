299fd3638cd974f289540dc314c1f9c638c01321
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-07-04 10:43:07 +0600

    explore page

---
+++ app/(site)/explore/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+
+
+const Explore = () => {
+    return (
+        <>
+            <Header showBackArrow label="Explore" />
+            <div className="flex items-center justify-center text-neutral-400">
+                <p>
+                    Page is under development
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default Explore;
